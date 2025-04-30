fe2d58352b502a3e7daab02a1d6df653f3bfb33a
Erlinrsa <erlinmariska2@gmail.com>
2025-04-30 18:36:41 +0700

    updt

---
+++ Modul 4/Codelab/src/perpustakaan/Anggota.java
@@ -0,0 +1,26 @@
+package perpustakaan;
+
+public class Anggota implements Peminjaman {
+    private String nama;
+    private String idAnggota;
+
+    public Anggota(String nama, String idAnggota) {
+        this.nama = nama;
+        this.idAnggota = idAnggota;
+        System.out.println("Anggota: " + nama + " (ID: " + idAnggota + ")");
+    }
+
+    // Overloading
+    public void pinjamBuku(String judul) {
+        System.out.println(nama + " meminjam buku berjudul: " + judul);
+    }
+
+    public void pinjamBuku(String judul, int durasi) {
+        System.out.println(nama + " meminjam buku \"" + judul + "\" selama " + durasi + " hari.");
+    }
+
+    @Override
+    public void kembalikanBuku(String judul) {
+        System.out.println(nama + " mengembalikan buku berjudul: " + judul);
+    }
+}
